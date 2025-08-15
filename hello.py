5938d9557905bcc06638eba65e590e2359d8395b
emaridavis <s3806867@students.fscj.edu>
2025-08-14 21:15:13 -0400

    added new message

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 # Emari Davis
 # 08/14/2025
+# a simple python program to print a message
+
+# Emari Davis
+# 08/14/2025
+# added new message
 
 print("Hey, Guys!")
+
+print("Welcome to my GitHub")
